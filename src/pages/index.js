78d634d79bbfdf2a7fac8730433a54932535e02e
Andy Wang <andywang@Andys-MacBook-Pro.local>
2023-02-03 13:32:34 -0800

    changed settings and about

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,34 @@
 import React from "react";
 import {
-  AboutSection,
-  ArticlesSection,
-  ContactSection,
-  HeroSection,
-  //InterestsSection,
-  Page,
-  ProjectsSection,
-  Seo,
+	AboutSection,
+	ArticlesSection,
+	ContactSection,
+	HeroSection,
+	InterestsSection,
+	Page,
+	ProjectsSection,
+	Seo,
 } from "gatsby-theme-portfolio-minimal";
 
 export default function IndexPage() {
-  return (
-    <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
-      <Page useSplashScreenAnimation>
-        <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        
-        <ProjectsSection sectionId="features" heading="Projects" />
-        <ContactSection sectionId="github" heading="Issues?" />
-      </Page>
-    </>
-  );
+	return (
+		<>
+			<Seo title="Gatsby Starter for Portfolio Minimal" />
+			<Page useSplashScreenAnimation>
+				<HeroSection sectionId="hero" />
+
+				<AboutSection sectionId="about" heading="About Portfolio Minimal" />
+				<InterestsSection sectionId="details" heading="Details" />
+				<ProjectsSection sectionId="projects" heading="Projects" />
+				<ContactSection sectionId="github" heading="Issues?" />
+			</Page>
+		</>
+	);
 }
 
 // <InterestsSection sectionId="details" heading="Details" />
+//<ArticlesSection
+// 					sectionId="articles"
+// 					heading="Latest Articles"
+// 					sources={["Medium"]}
+// 				/>
